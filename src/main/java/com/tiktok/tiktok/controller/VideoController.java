37749456d5ac8aa565e5b19446cdf2ba6c0e3d3d
TsvetelinaEvtimova <cvetelina_evtimova02@abv.bg>
2023-04-14 23:15:49 +0300

    Delete video added

--- src/main/java/com/tiktok/tiktok/controller/VideoController.java
+++ src/main/java/com/tiktok/tiktok/controller/VideoController.java
@@ -1,19 +1,18 @@
 package com.tiktok.tiktok.controller;
 
-import com.tiktok.tiktok.model.entities.Video;
 import com.tiktok.tiktok.service.VideoService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class VideoController extends AbstractController{
     @Autowired
     private VideoService videoService;
+    @DeleteMapping("/videos/{videoId}")
+    public DeleteVideoDTO deleteVideo(@PathVariable int videoId, HttpSession s){
+        int loggedUserId = getLoggedUserId(s);
+        return videoService.deleteVideo(videoId, loggedUserId);
+    }
 
 }
--- src/main/java/com/tiktok/tiktok/service/VideoService.java
+++ src/main/java/com/tiktok/tiktok/service/VideoService.java
@@ -1,15 +1,24 @@
 package com.tiktok.tiktok.service;
 
-import com.tiktok.tiktok.model.entities.User;
 import com.tiktok.tiktok.model.entities.Video;
-import com.tiktok.tiktok.model.exceptions.BadRequestException;
+import com.tiktok.tiktok.model.exceptions.NotFoundException;
+import com.tiktok.tiktok.model.exceptions.UnauthorizedException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class VideoService extends AbstractService{
 
-
+    public DeleteVideoDTO deleteVideo(int videoId, int loggedUserId) {
+        Optional<Video> video = videoRepository.findById(videoId);
+        if (!video.isPresent()){
+            throw new NotFoundException("Video not found");
+        }
+        if (video.get().getOwner().getId() != loggedUserId){
+            throw new UnauthorizedException("Can't delete this video");
+        }
+        videoRepository.deleteById(videoId);
+        return new DeleteVideoDTO();
+    }
 }
