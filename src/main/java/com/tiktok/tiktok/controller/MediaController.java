ea44f2bd2833b5c5710f2c401d98717618e56ff4
TsvetelinaEvtimova <cvetelina_evtimova02@abv.bg>
2023-04-14 21:23:29 +0300

    Upload video, bug fixed

--- src/main/java/com/tiktok/tiktok/controller/MediaController.java
+++ src/main/java/com/tiktok/tiktok/controller/MediaController.java
@@ -1,11 +1,9 @@
 package com.tiktok.tiktok.controller;
 
-<<<<<<< HEAD
-import com.tiktok.tiktok.model.DTOs.UserWithoutPassDTO;
+
+import com.tiktok.tiktok.model.DTOs.VideoSimpleDTO;
 import com.tiktok.tiktok.model.entities.Video;
-=======
 import com.tiktok.tiktok.model.DTOs.UserSimpleDTO;
->>>>>>> cdc216b247d57d29cde4b2f0d5f51c76de114135
 import com.tiktok.tiktok.service.MediaService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.time.LocalDateTime;
 
 @RestController
 public class MediaController extends AbstractController{
@@ -30,11 +26,11 @@ public class MediaController extends AbstractController{
         return mediaService.upload(file, id);
     }
     @PostMapping("/videos/media")
-    public Video uploadVideo(@RequestParam(value = "file") MultipartFile file,
-                             @RequestParam(value = "caption") String caption,
-                             @RequestParam(value = "isPrivate") Boolean isPrivate,
-                             @RequestParam(value = "soundId") int soundId,
-                             HttpSession s) {
+    public VideoSimpleDTO uploadVideo(@RequestParam("file") MultipartFile file,
+                                      @RequestParam("caption") String caption,
+                                      @RequestParam("isPrivate") Boolean isPrivate,
+                                      @RequestParam("soundId") int soundId,
+                                      HttpSession s) {
         int ownerId = getLoggedUserId(s);
         return mediaService.uploadVideo(ownerId, file, caption, isPrivate, soundId);
     }
---
+++ src/main/java/com/tiktok/tiktok/model/DTOs/VideoSimpleDTO.java
@@ -0,0 +1,37 @@
+package com.tiktok.tiktok.model.DTOs;
+
+import com.tiktok.tiktok.model.entities.Comment;
+import com.tiktok.tiktok.model.entities.Sound;
+import com.tiktok.tiktok.model.entities.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class VideoSimpleDTO {
+
+    private int id;
+
+    private String caption;
+
+    private LocalDateTime createdAt;
+
+    private boolean isPrivate;
+
+    private String url;
+
+    private UserSimpleDTO owner;
+
+    private SoundSimpleDTO sound;
+
+    private List<Comment> comments;
+}
