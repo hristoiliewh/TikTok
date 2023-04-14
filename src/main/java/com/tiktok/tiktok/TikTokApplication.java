95409bf253163a3e50011206ea22fbb392b17897
Hristo Iliev <hristoiliewh@Hristos-MacBook-Pro.local>
2023-04-14 12:50:36 +0300

    Project with directories uploaded

--- src/main/java/com/tiktok/tiktok/TikTokApplication.java
+++ src/main/java/com/tiktok/tiktok/TikTokApplication.java
@@ -1,13 +1,22 @@
 package com.tiktok.tiktok;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class TikTokApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TikTokApplication.class, args);
+    }
+
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
