8fc0acc3022e13c951a6e85b6c10911f4a67826a
TsvetelinaEvtimova <cvetelina_evtimova02@abv.bg>
2023-04-22 16:07:30 +0300

    Added unit tests for SoundService

--- src/test/java/com/tiktok/tiktok/SoundServiceTests.java
+++ src/test/java/com/tiktok/tiktok/SoundServiceTests.java
@@ -1,141 +1,93 @@
 package com.tiktok.tiktok;
 
-import com.tiktok.tiktok.controller.SoundController;
 import com.tiktok.tiktok.model.DTOs.soundsDTOs.SoundDTO;
 import com.tiktok.tiktok.model.entities.Sound;
+import com.tiktok.tiktok.model.exceptions.NotFoundException;
 import com.tiktok.tiktok.model.repositories.SoundRepository;
 import com.tiktok.tiktok.service.SoundService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class SoundServiceTests {
 
-    @Mock
-    private SoundService soundService;
-
     @InjectMocks
-    private SoundController soundController;
+    private SoundService soundService;
 
     @Mock
     private SoundRepository soundRepository;
 
-    private MockMvc mockMvc;
+    @Mock
+    private ModelMapper mapper;
+
+    private SoundDTO soundDTO;
 
     @BeforeEach
     public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(soundController).build();
-        mockMvc = MockMvcBuilders.standaloneSetup(soundRepository).build();
-        mockMvc = MockMvcBuilders.standaloneSetup(soundService).build();
+        MockitoAnnotations.openMocks(this);
+
+        soundDTO = new SoundDTO();
+        soundDTO.setId(1);
+        soundDTO.setName("Test Sound");
+        soundDTO.setUrl("uploads/809b6510-65af-41fd-8bdc-e0ba606fec72.mp3");
     }
 
     @Test
-    public void testGetByIdWithValidInput() {
-
-        // Arrange
+    public void testGetByIdSoundExists() {
         Sound sound = new Sound();
         sound.setId(1);
-        sound.setName("Sound1");
-        sound.setUrl("uploads/443a8493-96fd-41e7-89b2-a6441d9314f2.mp3");
-
+        // Mocking the soundRepository to return a Sound object when getById is called
+        when(soundRepository.findById(1)).thenReturn(Optional.of(sound));
+        // Mocking the mapper to return a SoundDTO object when mapping the Sound object
+        when(mapper.map(sound, SoundDTO.class)).thenReturn(soundDTO);
         // Act
-        SoundDTO expected = new SoundDTO();
-        expected.setId(1);
-        expected.setName("Sound1");
-        expected.setUrl("uploads/443a8493-96fd-41e7-89b2-a6441d9314f2.mp3");
-
-//        when(soundRepository.findById(1)).thenReturn(expected);
+        SoundDTO result = soundService.getById(1);
         // Assert
-        assertNotNull(expected);
-        Assertions.assertEquals(1, expected.getId());
-        Assertions.assertEquals("Sound1", expected.getName());
-    }
-    /*
-
-    @Test
-    public void testGetAllSoundsExist() {
-        // Arrange
-        Sound sound1 = new Sound();
-        sound1.setId(1);
-        sound1.setUrl("uploads/443a8493-96fd-41e7-89b2-a6441d9314f2.mp3");
-        sound1.setName("Sound1");
-        Sound sound2 = new Sound();
-        sound2.setId(2);
-        sound2.setName("Sound2");
-        sound2.setUrl("uploads/443a8493-96fd-41e7-89b2-a6441d9314f9.mp3");
-
-//        List<Sound> sounds = Arrays.asList(sound1, sound2);
-//        when(soundRepository.findAll()).thenReturn(sounds);
-
-        // Act
-        List<SoundSimpleDTO> expected = new ArrayList<>();
-
-        SoundSimpleDTO s1 = new SoundSimpleDTO();
-        s1.setId(1);
-        s1.setUrl("uploads/443a8493-96fd-41e7-89b2-a6441d9314f2.mp3");
-        s1.setName("Sound1");
-
-        SoundSimpleDTO s2 = new SoundSimpleDTO();
-        s1.setId(2);
-        s1.setUrl("uploads/443a8493-96fd-41e7-89b2-a6441d9314f9.mp3");
-        s1.setName("Sound2");
-
-        expected.add(s1);
-        expected.add(s2);
-
-        // Assert
-        assertNotNull(expected);
-        Assertions.assertEquals(2, expected.size());
-        Assertions.assertEquals("Sound1", expected.get(0).getName());
-        Assertions.assertEquals("Sound2", expected.get(1).getName());
+        assertEquals(soundDTO.getId(), result.getId());
+        verify(soundRepository, times(1)).findById(1);
+        verify(mapper, times(1)).map(sound, SoundDTO.class);
     }
 
     @Test
-    void testGetAllSoundsDoesNotExistThrowNotFoundException() {
-        // Arrange
-        when(soundRepository.findAll()).thenReturn(Arrays.asList());
-
-        // Act & Assert
-        Assertions.assertThrows(NotFoundException.class, () -> soundService.getAll());
+    public void testGetByIdSoundNotFound() {
+        // Mocking the soundRepository to return an empty optional when getById is called
+        when(soundRepository.findById(anyInt())).thenReturn(Optional.empty());
+        // Calling getById should throw a NotFoundException
+        assertThrows(NotFoundException.class, () -> soundService.getById(1));
     }
 
     @Test
-    void testGetByNameSoundExists() {
-        // Arrange
-        Sound sound1 = new Sound();
-        sound1.setId(1);
-        sound1.setName("Sound1");
-        Sound sound2 = new Sound();
-        sound2.setId(2);
-        sound2.setName("Sound2");
-        List<Sound> sounds = Arrays.asList(sound1, sound2);
-        when(soundRepository.findAllContains("ound")).thenReturn(sounds);
-        // Act
-        List<SoundSimpleDTO> soundSimpleDTOList = soundService.getByName("ound");
-
-        // Assert
-        Assertions.assertEquals(2, soundSimpleDTOList.size());
-        Assertions.assertEquals("Sound1", soundSimpleDTOList.get(0).getName());
-        Assertions.assertEquals("Sound2", soundSimpleDTOList.get(1).getName());
+    public void testGetAllSoundsFound() {
+        // Mocking the soundRepository to return a Page object with one Sound object when findAll is called
+        when(soundRepository.findAll(any(PageRequest.class))).thenReturn(new PageImpl<>(Arrays.asList(new Sound())));
+        // Mocking the mapper to return a SoundDTO object when mapping the Sound object
+        when(mapper.map(any(Sound.class), eq(SoundDTO.class))).thenReturn(soundDTO);
+        // Calling getAll should return a list with one mapped SoundDTO object
+        List<SoundDTO> result = soundService.getAll(0, 10);
+        assertEquals(1, result.size());
+        assertEquals(soundDTO, result.get(0));
     }
 
     @Test
-    void testGetByNameSoundDoesNotExistThrowNotFoundException() {
-        // Arrange
-        when(soundRepository.findAllContains("sound")).thenReturn(Arrays.asList());
-
-        // Act & Assert
-        Assertions.assertThrows(NotFoundException.class, () -> soundService.getByName("ound"));
+    public void testGetAllSoundsNotFound() {
+        // Mocking the soundRepository to return an empty Page object when findAll is called
+        when(soundRepository.findAll(any(PageRequest.class))).thenReturn(Page.empty());
+        // Calling getAll should throw a NotFoundException
+        assertThrows(NotFoundException.class, () -> soundService.getAll(0, 10));
     }
-
-     */
 }
